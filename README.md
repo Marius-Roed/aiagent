5c2b9941ab9303c4795cc523a5f22bbaf8a249a3
Marius-Roed <mwroed@gmail.com>
2025-07-05 12:59:24 +0200

    feat: ask gemini about bootdev

---
+++ README.md
@@ -0,0 +1,10 @@
+# aiAgent
+
+aiAgent is a cli wrapper for Google Gemini's 2.0 flash model
+
+### Usage
+
+This is built using `uv`, so you need to have that installed as well as python 3.10+
+
+1. `$ source .venv/bin/activate`
+2. `$ uv run main.py`
--- main.py
+++ main.py
@@ -1,5 +1,23 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+
+
 def main():
-    print("Hello from aiagent!")
+    load_dotenv()
+    api_key = os.environ.get('GEMINI_API_KEY')
+
+    client = genai.Client(api_key=api_key)
+
+    resp = client.models.generate_content(
+        model='gemini-2.0-flash-001',
+        contents="Why is Boot.dev such a great place to learn backend developement. Use one paragraph maximum"
+    )
+
+    print(resp.text)
+
+    print("Proompt tokens: " + resp.usage_metadata.prompt_token_count)
+    print("Resp tokens: " + resp.usage_metadata.candidates_token_count)
 
 
 if __name__ == "__main__":
